969b4cba23e5d1249ab4c75fe36df689dc7d154f
Keaganvolkwyn <126804154+Keaganvolkwyn@users.noreply.github.com>
2024-01-14 20:39:34 +0200

    Add files via upload

---
+++ travel journal/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import Data from "./data"
+
+export default function App() {
+    const details = Data.map((detail) => {
+        return <Card 
+            key={detail.id}
+            detail={detail}
+        />
+    })
+    return (
+        <div className="container">
+            <Navbar />
+            <section className="card-list">
+                {details}
+            </section>
+        </div>
+    )
+}
